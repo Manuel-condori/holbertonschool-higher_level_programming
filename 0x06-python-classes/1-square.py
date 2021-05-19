9c4f5625f9ef64fff6326cd44d1db94793613e35
Manuel-condori <2828@holbertonschool.com>
2021-05-18 21:00:40 -0500

    square-1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    def __init__(self, size):
+
+        """Attributes:
+                size: size of the square
+        """
+
+        self.__size = size
