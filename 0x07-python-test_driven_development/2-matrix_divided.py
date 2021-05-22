8dfbaa4c2372c3b4bf9f1610b6a0eda1a4b3fad0
Manuel-condori <2828@holbertonschool.com>
2021-05-21 22:44:12 -0500

    correction of spaces

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+
 """This module holds the function matrix_divided which takes a matrix
 and divides each element by 3
 """
+
+
 def matrix_divided(matrix, div):
+
     """Divides the matrix"""
     err = 'matrix must be a matrix (list of lists) of integers/floats'
     if type(div) != int and type(div) != float:
@@ -23,7 +27,7 @@ def matrix_divided(matrix, div):
     if any(not row for row in matrix):
         raise TypeError(err)
 
-    l = len(matrix[0])
-    if any(len(row) != l for row in matrix):
+    lenght = len(matrix[0])
+    if any(len(row) != lenght for row in matrix):
         raise TypeError('Each row of the matrix must have the same size')
     return [[round(col / div, 2) for col in row] for row in matrix]
