6fdc30ce3330bd15959130c84669b20a00d7fbdd
Manuel-condori <2828@holbertonschool.com>
2021-06-01 23:28:25 -0500

    inherits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""Module holds a function that determines if an object is an instance of a
+class that inherited from a specified class
+"""
 
 
 def inherits_from(obj, a_class):
+    """Args:
+        obj: The object to check
+        a_class: The class to check against
+    """
     if type(obj) == a_class:
         return False
     return isinstance(obj, a_class)
