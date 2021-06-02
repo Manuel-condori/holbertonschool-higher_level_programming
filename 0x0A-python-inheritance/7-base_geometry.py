05b6ba49549093a40692482f47e1cc86452e21d5
Manuel-condori <2828@holbertonschool.com>
2021-06-01 23:36:58 -0500

    add coments

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-
+"""Module that holds the class BaseGeometry
+"""
 
 class BaseGeometry:
+    """Class that defines a shape
+    """
 
     def area(self):
+        """Function that calculates the area of a shapre
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Function that validates input
+        """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
