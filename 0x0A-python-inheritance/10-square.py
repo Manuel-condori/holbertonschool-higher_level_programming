7019270021b92762fa0e21aee9641f896be7a6c5
Manuel-condori <2828@holbertonschool.com>
2021-06-01 23:49:16 -0500

    add coments

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""Module that holds the class BaseGeometry
+"""
+
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Class that defines a shape
+    """
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
