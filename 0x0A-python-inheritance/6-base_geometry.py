7d2e5a4c06816d7dd75a26dacaa94311f00c33c5
Manuel-condori <2828@holbertonschool.com>
2021-06-01 23:34:24 -0500

    add coments

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""Module that has an empty base class BaseGeometry and empty class function
+"""
 
 
 class BaseGeometry:
+    """Class that defines a shape
+    """
+
     def area(self):
+        """Function that calculates the area of a shapre
+        """
         raise Exception("area() is not implemented")
