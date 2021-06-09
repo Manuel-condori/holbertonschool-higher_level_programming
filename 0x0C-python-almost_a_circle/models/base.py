41e648c56887119187d91f37826468d9244fab6f
Manuel-condori <2828@holbertonschool.com>
2021-06-08 22:19:28 -0500

    json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,4 +24,4 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
-            return json.dumps(list_dictionaries)            
+            return json.dumps(list_dictionaries)
