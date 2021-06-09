7aa908284587fb0e8f8eb0589571dadce1682a7a
Manuel-condori <2828@holbertonschool.com>
2021-06-08 22:30:41 -0500

    delet spaces

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,4 +23,5 @@ class Base:
         """ returns JSON of a dictionary """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return (json.dumps(list_dictionaries))
