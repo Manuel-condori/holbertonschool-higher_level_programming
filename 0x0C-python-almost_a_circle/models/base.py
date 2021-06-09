2f9e8932d551f2b657f7d8b28f18c54d9ef51f37
Manuel-condori <2828@holbertonschool.com>
2021-06-08 19:26:29 -0500

    first commit base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module holds class Base"""
+import json
+
+
+class Base:
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes attributes for class Base
+        Args:
+            id: public instance attribute
+        """
+
+        if id is not None:
+            self.id = id
+        else:    
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
