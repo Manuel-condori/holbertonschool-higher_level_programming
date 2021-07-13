f7c266e42e775b196bac6ce8b9b01682cd2d54df
Manuel condori <2828@holbertonschool.com>
2021-07-13 08:07:19 +0000

    add file first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,5 @@
---creates a table in current database
-CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
+--lists tables [id] [name]
+-- show all tables
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256));
