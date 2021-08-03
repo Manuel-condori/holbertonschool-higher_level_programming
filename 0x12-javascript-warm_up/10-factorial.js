05aa4bb52ed964aea86981aa942db76a02d5558d
Manuel-condori <2828@holbertonschool.com>
2021-08-02 23:40:15 -0500

    add file script that compute and print a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// js script thatt computes and prints a factorial
+
+const n = parseInt(process.argv[2]);
+function factorial (n) {
+  if (isNaN(n)) {
+    return (1);
+  } else {
+    if (n === 0) {
+      return (1);
+    }
+    return (n * factorial(n - 1));
+  }
+}
+console.log(factorial(n));
