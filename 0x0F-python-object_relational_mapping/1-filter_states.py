818f164afd6c6d8d60dc3ac01bcb3db3bc9b98f7
Manuel-condori <2828@holbertonschool.com>
2021-08-08 20:14:41 -0500

    add file that lists all states with a name starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name
+starting with N (upper N) from the database
+hbtn_0e_0_usa
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    HOST = "localhost"
+    PORT = 3306
+    USER = argv[1]
+    PASSWORD = argv[2]
+    DATABASE = argv[3]
+    db = MySQLdb.connect(host=HOST, user=USER, password=PASSWORD,
+                         db=DATABASE, port=PORT)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id")
+    rows = cursor.fetchall()
+    for r in rows:
+        if r[1][0] == 'N':
+            print(r)
+    cursor.close()
+    db.close()
