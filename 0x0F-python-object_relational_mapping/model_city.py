a869b74c3ede9266e867f7c3af93d53795787da6
Manuel-condori <2828@holbertonschool.com>
2021-08-10 15:20:34 -0500

    first commit model city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""file that contains the class definition of a City"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+
+class City(Base):
+    """Class City declaration"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    state_id = Column(Integer, ForeignKey('states.id'))
